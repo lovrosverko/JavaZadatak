c59788a3166f29c246a5c67397ddc12d41232709
Lovro Šverko <lovro.sverko@sser.hr>
2018-11-22 22:56:58 +0100

    Producer/consumer radi
    to do:
    parse stringa val
    konverzija u xml

--- Thread/src/Threadexample.java
+++ Thread/src/Threadexample.java
@@ -1,5 +1,4 @@
-// Java program to implement solution of producer 
-// consumer problem. 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
@@ -10,11 +9,10 @@ public class Threadexample
 	public static void main(String[] args) 
 						throws InterruptedException 
 	{ 
-		// Object of a class that has both produce() 
-		// and consume() methods 
-		final PC pc = new PC(); 
 
-		// Create producer thread 
+		final ProducerConsumer ProducerConsumer = new ProducerConsumer(); 
+
+
 		Thread t1 = new Thread(new Runnable() 
 		{ 
 			@Override
@@ -22,7 +20,7 @@ public class Threadexample
 			{ 
 				try
 				{ 
-					pc.produce(); 
+					ProducerConsumer.produce(); 
 				} 
 				catch(InterruptedException e) 
 				{ 
@@ -31,7 +29,7 @@ public class Threadexample
 			} 
 		}); 
 
-		// Create consumer thread 
+
 		Thread t2 = new Thread(new Runnable() 
 		{ 
 			@Override
@@ -39,7 +37,7 @@ public class Threadexample
 			{ 
 				try
 				{ 
-					pc.consume(); 
+					ProducerConsumer.consume(); 
 				} 
 				catch(InterruptedException e) 
 				{ 
@@ -57,91 +55,64 @@ public class Threadexample
 		t2.join(); 
 	} 
 
-	// This class has a list, producer (adds items to list 
-	// and consumber (removes items). 
-	public static class PC 
+	public static class ProducerConsumer 
 	{ 
-		// Create a list shared by producer and consumer 
-		// Size of list is 2. 
-		LinkedList<String[]> transakcija = new LinkedList<>(); 
-		int capacity = 2; 
+		 
+		LinkedList<String> transakcija = new LinkedList<>(); 
+		int kapacitet = 2; 
 		String datoteka = "//home//lovro//eclipse-workspace//testMAI//docs//HOCIDC.001";
-		String kontrola = "F";
-		int brojac2 = 0;
+		String token;
+
 		
-		// Function called by producer thread 
 		public void produce() throws InterruptedException 
 		{ 
-			String[] buffer = new String[12];
-			int brojac = 0;
+			
 			try (Scanner scanner = new Scanner(new File(datoteka));) 
 			{
-			scanner.useDelimiter("\n");
-			
-			while (scanner.hasNext()) {
-				String token = scanner.nextLine();
-				buffer = token.split(":");
-				brojac++;
-				transakcija.add(buffer);
-				if (buffer[6].equals(kontrola))
-					break;
-				
-			}
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-			while (true) 
-			{ 
-				synchronized (this) 
+				while (scanner.hasNext()) 
 				{ 
-					// producer thread waits while list 
-					// is full 
-					while (transakcija.size()==capacity) 
-						wait(); 
-
-					System.out.println("Producer produced-"
-												+ buffer[6]); 
-
-					// to insert the jobs in the list 
-					transakcija.add(buffer); 
-
-					// notifies the consumer thread that 
-					// now it can start consuming 
-					notify(); 
-
-					// makes the working of program easier 
-					// to understand 
-					Thread.sleep(1000); 
-				} 
-			} 
+					synchronized (this) 
+					{ 
+						token = scanner.nextLine();
+						while (transakcija.size()==kapacitet) 
+							wait();
+						System.out.println("Producer produced-"
+								+ token);
+						transakcija.add(token);
+						notify(); 
+					}
+				}
+			} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 		} 
 
 		// Function called by consumer thread 
 		public void consume() throws InterruptedException 
 		{ 
+			int brojac = 0;
 			while (true) 
 			{ 
 				synchronized (this) 
 				{ 
-					// consumer thread waits while list 
-					// is empty 
+
 					while (transakcija.size()==0) 
 						wait(); 
+					String val = transakcija.removeFirst(); 
+/*
+ * 
+ * ovdje sada trebam dodati konverziju u xml
+ * pogledati JAXB
+ * 
+ */
+					// debug
+					brojac++;
+					System.out.println(brojac + " Consumer consumed-"
+													+ val);
 
-					//to retrive the ifrst job in the list 
-					String[] val = transakcija.removeFirst(); 
-
-					for (int i = 0; i<val.length; i++) {
-					System.out.println("Consumer consumed-"
-													+ val[i]);
-					}
-
-					// Wake up producer thread 
 					notify(); 
 
-					// and sleep 
-					Thread.sleep(1000); 
 				} 
 			} 
 		} 
